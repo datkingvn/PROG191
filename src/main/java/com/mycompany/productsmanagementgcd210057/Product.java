50d6febe019202585fef24ba9d936827e9489a03
Mạnh Đạt <tranhuudats1@gmail.com>
2023-12-14 09:02:29 +0700

    :tada: UI/UX Product Management

---
+++ src/main/java/com/mycompany/productsmanagementgcd210057/Product.java
@@ -0,0 +1,73 @@
+package com.mycompany.productsmanagementgcd210057;
+
+/**
+ *
+ * @author tranmanhdat
+ */
+public class Product {
+    private String productID;
+    private String productName;
+    private int productQuantity;
+    private double productPrice;
+    private String productDescribe;
+    private String productCategory;
+
+    public Product(String productID, String productName, int productQuantity, double productPrice, String productDescribe, String productCategory) {
+        this.productID = productID;
+        this.productName = productName;
+        this.productQuantity = productQuantity;
+        this.productPrice = productPrice;
+        this.productDescribe = productDescribe;
+        this.productCategory = productCategory;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public void setProductDescribe(String productDescribe) {
+        this.productDescribe = productDescribe;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public String getProductDescribe() {
+        return productDescribe;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/productsmanagementgcd210057/ProductsManagementGCD210057.java
@@ -0,0 +1,16 @@
+package com.mycompany.productsmanagementgcd210057;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tranmanhdat
+ */
+public class ProductsManagementGCD210057 {
+
+    public static void main(String[] args) {
+        ProductJFrame productJFrame = new ProductJFrame();
+        productJFrame.show();
+    }
+}
